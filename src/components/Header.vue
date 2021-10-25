721cf7f532dc67607241daf7500ed24d5a527d1f
Reza Azerly <kiritosao080@gmail.com>
2021-10-25 14:06:06 +0700

    setup tailwind

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="bg-hero-image-mobile sm:bg-hero-image-desktop">
+    <nav class="w-3/4 mx-auto flex flex-row justify-between items-center">
+      <img src="../assets/images/logo.svg" alt="crowdfund logo">
+      <div id="wrapper">
+        <a href="#">About</a>
+        <a href="#">Discover</a>
+        <a href="#">Get Started</a>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
